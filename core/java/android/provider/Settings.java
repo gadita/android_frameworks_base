6ada0b20c598c4fcf9c62a7cadaf0169ee5deeec
Daveee10 <daver16@msn.com>
2012-08-04 07:41:25 +0200

    Add easter egg for Home button answers call

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1158,6 +1158,12 @@ public final class Settings {
         public static final String HOME_BUTTON_ANSWERS_CALL = "home_button_answers_call";
 
         /**
+         * Home button answers call easter egg :)
+         * @hide
+         */
+        public static final String ANSWERS_CALL_TRICK = "answers_call_trick";
+
+        /**
          * Reboot show/hide
          * @hide
          */
