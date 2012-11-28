c39d976b617b85a879261aed00e87f6dfa4e31fc
Daveee10 <daver16@msn.com>
2012-11-28 17:35:13 +0100

    Lockscreen branding depends on LCD density ([LDPI]JBMP / [MDPI]JB Mini Project)

--- policy/src/com/android/internal/policy/impl/LockScreen.java
+++ policy/src/com/android/internal/policy/impl/LockScreen.java
@@ -698,7 +698,10 @@ class LockScreen extends RelativeLayout implements KeyguardScreen {
         lp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
         mJBMPleft.setLayoutParams(lp);
         mJBMPleft.setVisibility(View.VISIBLE);
-        mJBMPleft.setText("JBMP " + android.os.SystemProperties.get("ro.modversion"));
+        if (Integer.parseInt(android.os.SystemProperties.get("ro.sf.lcd_density")) == 120)
+            mJBMPleft.setText("JBMP " + android.os.SystemProperties.get("ro.modversion"));
+        else
+            mJBMPleft.setText("JB Mini Project " + android.os.SystemProperties.get("ro.modversion"));
         mJBMPleft.setTextColor(0xffffffff);
         this.addView(mJBMPleft);
 
