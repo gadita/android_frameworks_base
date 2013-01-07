0c4175b655be85b61cfb5b0499019824ebf0a173
Daveee10 <daver16@msn.com>
2013-01-07 14:28:39 +0100

    Use Landscape lockscreen only with original JB lockscreen style

--- policy/src/com/android/internal/policy/impl/LockScreen.java
+++ policy/src/com/android/internal/policy/impl/LockScreen.java
@@ -668,13 +668,7 @@ class LockScreen extends RelativeLayout implements KeyguardScreen {
             else
                 inflater.inflate(R.layout.keyguard_screen_tab_unlock, this, true);
         } else {
-            if (mUseSlider)
-                inflater.inflate(R.layout.keyguard_screen_slider_unlock_land, this, true);
-            else if (mUseRotary)
-                inflater.inflate(R.layout.keyguard_screen_rotary_unlock_land, this, true);
-            else if (mUseHoneyComb)
-                inflater.inflate(R.layout.keyguard_screen_honeycomb_unlock_land, this, true);
-            else
+            if (!mUseSlider || !mUseRotary || !mUseHoneyComb)
                 inflater.inflate(R.layout.keyguard_screen_tab_unlock_land, this, true);
         }
 
